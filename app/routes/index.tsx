5590b01cc99c94dfd424110b82ae8d7dca6d1bee
nykka <veronicaines.garcia@gmail.com>
2022-05-10 20:15:18 -0300

    Add image

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,15 @@
+import image from "../images/kirby.webp";
+
 export default function Index() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="text-center flex flex-col items-center">
+      <h1>I'm Nykka</h1>
+      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+        <img 
+          src={image} 
+          alt="Kirby sitting on a folding bench with a fishing rod fishing in a river. It is wearing a bucket hat and it is smiling." 
+        />
+      </div>
     </div>
   );
 }
