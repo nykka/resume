b67db3f824ae163417b458974fb68d98277891ef
nykka <veronicaines.garcia@gmail.com>
2022-05-18 00:05:29 -0300

    Add basic structure, nav and images with alt

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LoaderFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -6,29 +6,58 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData
 } from "@remix-run/react";
+
+import { NavList } from "./components/nav";
 
 import styles from "./styles/app.css"
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Veronica Garcia Resume",
   viewport: "width=device-width,initial-scale=1",
 });
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }]
-}
+  return [
+    { rel: "stylesheet", href: styles }, 
+    { rel: "preconnect", href: "https://fonts.googleapis.com" }, 
+    { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "true" }, 
+    { 
+      rel: "stylesheet", 
+      href: "https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;800&display=swap"
+    }
+  ]
+};
+
+export const loader: LoaderFunction = async ({
+  request,
+}) => {
+  const url = new URL(request.url);
+  const theme = url.searchParams.get("theme");
+  return theme;
+};
 
 export default function App() {
+
+  const theme = useLoaderData();
+
   return (
-    <html lang="en">
+    <html lang="en" data-theme={theme === "light" ? "light" : "black"}>
       <head>
         <Meta />
         <Links />
       </head>
       <body>
-        <Outlet />
+        <div className="flex flex-col flex-wrap items-center">
+          <div className="flex flex-col flex-wrap items-center w-screen max-w-7xl py-2">
+            <NavList />
+            <div className="max-w-6xl">
+              <Outlet />
+            </div>
+          </div>
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,25 @@
-import image from "../images/kirby.webp";
+import { Link } from "@remix-run/react";
+import image from "../images/nykka.png";
 
 export default function Index() {
   return (
-    <div className="text-center flex flex-col items-center">
-      <h1>I'm Nykka</h1>
-      <div className="card card-compact w-96 bg-base-100 shadow-xl">
-        <img 
-          src={image} 
-          alt="Kirby sitting on a folding bench with a fishing rod fishing in a river. It is wearing a bucket hat and it is smiling." 
-        />
+    <div className="py-20">
+      <div className="card w-full text-left flex flex-col flex-wrap items-left">
+        <div className="card-body items-left text-left w-full">
+          <div className="avatar">
+            <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <img 
+                src={image} 
+                alt="White and black cartoon of myself. I have both of my hands over each sides of my face, my eyes are closed and I have an expression of daydreaming about something really cute." 
+              />
+            </div>
+          </div>
+          {/* <p className="font-bold">About me</p> */}
+          <h1 className="py-2 text-6xl font-bold">Hi! I’m Vero ~ aka Nykka, and my pronouns are she/her.</h1>
+          <p className="py-2 text-2xl">I'm a <strong>Front End Developer</strong> with a focus on web/web app development, pixel perfection, UX/UI, and accessibility. I like to work in diverse environments where caring for the best practices and teamwork are priorities. </p>
+          <p className="py-2 text-2xl">I love to participate in communities whose mission is to spread and share knowledge, especially to minorities and underrepresented communities. I’m actually a staff member in FrontendCafé a tech server in Discord. <Link to="https://discord.gg/2P99cTQZ" target="_blank" className="link hover:no-underline">Click here to join the server.</Link></p>
+          <p className="py-2 text-2xl">In my free time I write and draw my own comic books. If you are interested in them, you can <Link className="link hover:no-underline" to="/comics">click here to read my comics.</Link></p>
+        </div>
       </div>
     </div>
   );
