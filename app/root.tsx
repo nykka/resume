e6b04508f345ee733d7593081323efe6e6ea5136
nykka <veronicaines.garcia@gmail.com>
2022-06-03 10:59:05 -0300

    improve navigation

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 	ScrollRestoration,
 	useLoaderData
 } from "@remix-run/react";
+import { useEffect, useState } from "react";
 
 import { NavList } from "./components/nav";
 
@@ -42,7 +43,26 @@ export const loader: LoaderFunction = async ({
 
 export default function App() {
 
-	const theme = useLoaderData();
+	const theme: string = useLoaderData();
+
+	const [subMenuName, setSubMenuName] = useState<string | null>();
+
+	function handleClick(e: React.MouseEvent<HTMLButtonElement>){
+		const target = e.currentTarget;
+		const ariaControls = target.getAttribute("aria-controls");
+		const ariaExpanded = target.getAttribute("aria-expanded");
+		ariaControls === subMenuName ? setSubMenuName(null) : setSubMenuName(ariaControls);
+		ariaExpanded === "true" ? target.setAttribute("aria-expanded", "false") : target.setAttribute("aria-expanded", "true");
+	};
+
+	useEffect(() => {
+		const body = document.querySelector("body");
+		const closeSubmenu = () => {
+			setSubMenuName(null);
+		};
+		body?.addEventListener("click", closeSubmenu);
+		return () => body?.removeEventListener("click", closeSubmenu);
+	}, []);
 
 	return (
 		<html lang="en" data-theme={theme === "light" ? "light" : "black"}>
@@ -55,7 +75,7 @@ export default function App() {
 					<div className="flex flex-col flex-wrap items-center w-screen max-w-7xl py-2">
 						<Link className="sr-only absolute badge px-2 focus:not-sr-only focus:px-2 focus:absolute focus:h-5" to="#maincontent">Skip to main content</Link>
 						<header className="max-w-6xl w-full">
-							<NavList />
+							<NavList theme={theme} subMenuName={subMenuName} handleClick={handleClick} />
 						</header>
 						<main id="maincontent" className="max-w-6xl">
 							<Outlet />
